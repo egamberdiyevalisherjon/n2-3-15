db5f38d2ae736749a7ad64ba10f49c9d401c1c77
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-11-07 10:40:20 +0500

    intial commit

---
+++ js/app.js
@@ -0,0 +1,369 @@
+// let users = [
+//   {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//       street: "Kulas Light",
+//       suite: "Apt. 556",
+//       city: "Gwenborough",
+//       zipcode: "92998-3874",
+//       geo: {
+//         lat: "-37.3159",
+//         lng: "81.1496",
+//       },
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//       name: "Romaguera-Crona",
+//       catchPhrase: "Multi-layered client-server neural-net",
+//       bs: "harness real-time e-markets",
+//     },
+//   },
+//   {
+//     id: 2,
+//     name: "Ervin Howell",
+//     username: "Antonette",
+//     email: "Shanna@melissa.tv",
+//     address: {
+//       street: "Victor Plains",
+//       suite: "Suite 879",
+//       city: "Wisokyburgh",
+//       zipcode: "90566-7771",
+//       geo: {
+//         lat: "-43.9509",
+//         lng: "-34.4618",
+//       },
+//     },
+//     phone: "010-692-6593 x09125",
+//     website: "anastasia.net",
+//     company: {
+//       name: "Deckow-Crist",
+//       catchPhrase: "Proactive didactic contingency",
+//       bs: "synergize scalable supply-chains",
+//     },
+//   },
+//   {
+//     id: 3,
+//     name: "Clementine Bauch",
+//     username: "Samantha",
+//     email: "Nathan@yesenia.net",
+//     address: {
+//       street: "Douglas Extension",
+//       suite: "Suite 847",
+//       city: "McKenziehaven",
+//       zipcode: "59590-4157",
+//       geo: {
+//         lat: "-68.6102",
+//         lng: "-47.0653",
+//       },
+//     },
+//     phone: "1-463-123-4447",
+//     website: "ramiro.info",
+//     company: {
+//       name: "Romaguera-Jacobson",
+//       catchPhrase: "Face to face bifurcated interface",
+//       bs: "e-enable strategic applications",
+//     },
+//   },
+//   {
+//     id: 4,
+//     name: "Patricia Lebsack",
+//     username: "Karianne",
+//     email: "Julianne.OConner@kory.org",
+//     address: {
+//       street: "Hoeger Mall",
+//       suite: "Apt. 692",
+//       city: "South Elvis",
+//       zipcode: "53919-4257",
+//       geo: {
+//         lat: "29.4572",
+//         lng: "-164.2990",
+//       },
+//     },
+//     phone: "493-170-9623 x156",
+//     website: "kale.biz",
+//     company: {
+//       name: "Robel-Corkery",
+//       catchPhrase: "Multi-tiered zero tolerance productivity",
+//       bs: "transition cutting-edge web services",
+//     },
+//   },
+//   {
+//     id: 5,
+//     name: "Chelsey Dietrich",
+//     username: "Kamren",
+//     email: "Lucio_Hettinger@annie.ca",
+//     address: {
+//       street: "Skiles Walks",
+//       suite: "Suite 351",
+//       city: "Roscoeview",
+//       zipcode: "33263",
+//       geo: {
+//         lat: "-31.8129",
+//         lng: "62.5342",
+//       },
+//     },
+//     phone: "(254)954-1289",
+//     website: "demarco.info",
+//     company: {
+//       name: "Keebler LLC",
+//       catchPhrase: "User-centric fault-tolerant solution",
+//       bs: "revolutionize end-to-end systems",
+//     },
+//   },
+//   {
+//     id: 6,
+//     name: "Mrs. Dennis Schulist",
+//     username: "Leopoldo_Corkery",
+//     email: "Karley_Dach@jasper.info",
+//     address: {
+//       street: "Norberto Crossing",
+//       suite: "Apt. 950",
+//       city: "South Christy",
+//       zipcode: "23505-1337",
+//       geo: {
+//         lat: "-71.4197",
+//         lng: "71.7478",
+//       },
+//     },
+//     phone: "1-477-935-8478 x6430",
+//     website: "ola.org",
+//     company: {
+//       name: "Considine-Lockman",
+//       catchPhrase: "Synchronised bottom-line interface",
+//       bs: "e-enable innovative applications",
+//     },
+//   },
+//   {
+//     id: 7,
+//     name: "Kurtis Weissnat",
+//     username: "Elwyn.Skiles",
+//     email: "Telly.Hoeger@billy.biz",
+//     address: {
+//       street: "Rex Trail",
+//       suite: "Suite 280",
+//       city: "Howemouth",
+//       zipcode: "58804-1099",
+//       geo: {
+//         lat: "24.8918",
+//         lng: "21.8984",
+//       },
+//     },
+//     phone: "210.067.6132",
+//     website: "elvis.io",
+//     company: {
+//       name: "Johns Group",
+//       catchPhrase: "Configurable multimedia task-force",
+//       bs: "generate enterprise e-tailers",
+//     },
+//   },
+//   {
+//     id: 8,
+//     name: "Nicholas Runolfsdottir V",
+//     username: "Maxime_Nienow",
+//     email: "Sherwood@rosamond.me",
+//     address: {
+//       street: "Ellsworth Summit",
+//       suite: "Suite 729",
+//       city: "Aliyaview",
+//       zipcode: "45169",
+//       geo: {
+//         lat: "-14.3990",
+//         lng: "-120.7677",
+//       },
+//     },
+//     phone: "586.493.6943 x140",
+//     website: "jacynthe.com",
+//     company: {
+//       name: "Abernathy Group",
+//       catchPhrase: "Implemented secondary concept",
+//       bs: "e-enable extensible e-tailers",
+//     },
+//   },
+//   {
+//     id: 9,
+//     name: "Glenna Reichert",
+//     username: "Delphine",
+//     email: "Chaim_McDermott@dana.io",
+//     address: {
+//       street: "Dayna Park",
+//       suite: "Suite 449",
+//       city: "Bartholomebury",
+//       zipcode: "76495-3109",
+//       geo: {
+//         lat: "24.6463",
+//         lng: "-168.8889",
+//       },
+//     },
+//     phone: "(775)976-6794 x41206",
+//     website: "conrad.com",
+//     company: {
+//       name: "Yost and Sons",
+//       catchPhrase: "Switchable contextually-based project",
+//       bs: "aggregate real-time technologies",
+//     },
+//   },
+//   {
+//     id: 10,
+//     name: "Clementina DuBuque",
+//     username: "Moriah.Stanton",
+//     email: "Rey.Padberg@karina.biz",
+//     address: {
+//       street: "Kattie Turnpike",
+//       suite: "Suite 198",
+//       city: "Lebsackbury",
+//       zipcode: "31428-2261",
+//       geo: {
+//         lat: "-38.2386",
+//         lng: "57.2232",
+//       },
+//     },
+//     phone: "024-648-3804",
+//     website: "ambrose.net",
+//     company: {
+//       name: "Hoeger LLC",
+//       catchPhrase: "Centralized empowering task-force",
+//       bs: "target end-to-end models",
+//     },
+//   },
+// ];
+
+// let row = document.querySelector(".cards");
+// let form = document.querySelector("form");
+
+// let displayUser = (user) => {
+//   let col = document.createElement("div");
+//   col.classList.add("col-lg-4");
+//   let card = document.createElement("div");
+//   card.classList.add("card");
+//   col.append(card);
+//   let cardHeader = document.createElement("div");
+//   cardHeader.classList.add("card-header");
+//   card.append(cardHeader);
+//   let cardBody = document.createElement("div");
+//   cardBody.classList.add("card-body");
+//   card.append(cardBody);
+//   let cardFooter = document.createElement("div");
+//   cardFooter.classList.add("card-footer");
+//   card.append(cardFooter);
+//   let nameElement = document.createElement("h3");
+//   nameElement.textContent = user.name;
+//   cardHeader.append(nameElement);
+//   let usernameElement = document.createElement("p");
+//   usernameElement.textContent = user.username;
+//   cardHeader.append(usernameElement);
+//   let emailLink = document.createElement("a");
+//   emailLink.setAttribute("href", "mailto:" + user.email);
+//   emailLink.innerText = user.email;
+//   cardBody.append(emailLink);
+//   let br1 = document.createElement("br");
+//   cardBody.append(br1);
+//   let phoneLink = document.createElement("a");
+//   phoneLink.setAttribute("href", "tel:" + user.phone);
+//   phoneLink.innerText = user.phone;
+//   cardBody.append(phoneLink);
+//   let br2 = document.createElement("br");
+//   cardBody.append(br2);
+//   let webLink = document.createElement("a");
+//   webLink.setAttribute("href", "https://" + user.website);
+//   webLink.innerText = user.website;
+//   cardBody.append(webLink);
+//   let company = document.createElement("div");
+//   cardBody.append(company);
+//   let companyName = document.createElement("h4");
+//   companyName.innerText = user.company.name;
+//   company.append(companyName);
+//   let companyPhrase = document.createElement("p");
+//   companyPhrase.innerText = user.company.catchPhrase;
+//   company.append(companyPhrase);
+//   let address = document.createElement("p");
+//   address.innerText = user.address.city.concat(
+//     ", ",
+//     user.address.street,
+//     ", ",
+//     user.address.suite,
+//     ", ",
+//     user.address.zipcode
+//   );
+//   cardFooter.append(address);
+//   row.append(col);
+// };
+
+// users.forEach(displayUser);
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
+
+// form[0].addEventListener("keydown", (e) => {
+//   let text = e.target.value;
+//   row.innerHTML = "";
+//   let filteredUsers = users.filter((user) =>
+//     user.name.toLowerCase().includes(text.toLowerCase())
+//   );
+//   filteredUsers.forEach(displayUser);
+// });
+
+// let salom = "Hammaga Salom!";
+// let h1 = document.querySelector("h1");
+
+// (function type(str) {
+//   let direction = "inc";
+//   setInterval(() => {
+//     if (h1.textContent.length === str.length) direction = "dec";
+//     else if (h1.textContent.length === 0) direction = "inc";
+
+//     if (direction === "inc") h1.textContent += str[h1.textContent.length];
+//     else h1.textContent = h1.textContent.slice(0, h1.textContent.length - 1);
+//   }, 300);
+// })(salom);
+
+// let form = document.querySelector("form");
+// let ul = document.querySelector("ul");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   let productName = e.target[0].value;
+//   if (!productName) return;
+
+//   let li = document.createElement("li");
+//   li.classList.add("list-group-item");
+//   li.innerText = productName;
+
+//   ul.append(li);
+//   e.target.reset()
+// });
+
+let input = document.querySelector("input");
+let btns = document.querySelectorAll("button");
+
+function evaluate() {
+  if (!input.value) return;
+  input.value = eval(input.value);
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("equal")) return evaluate();
+    if (e.target.classList.contains("clear")) return (input.value = "");
+    if (e.target.classList.contains("backspace"))
+      return (input.value = input.value.slice(0, input.value.length - 1));
+    input.value += e.target.innerText;
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  let char = e.key;
+  console.log(char);
+  if (char === "=" || char === "Enter") return evaluate();
+  if (char === "Backspace") {
+    if (e.altKey) return (input.value = "");
+    return (input.value = input.value.slice(0, input.value.length - 1));
+  }
+  if (["+", "-", "*", "/", "."].includes(char)) return (input.value += char);
+  if (isNaN(+char)) return;
+  input.value += char;
+});
